eb4c077b3173a30ab6596281379d92016346dacc
abdullah-NO <abdullah.hussain97@hotmail.com>
2023-02-27 13:56:14 +0100

    feature: added franchise and movie data models

--- MediaDatabaseCreator/MediaDatabaseCreator/Model/Franchise.cs
+++ MediaDatabaseCreator/MediaDatabaseCreator/Model/Franchise.cs
@@ -6,7 +6,9 @@ namespace MediaDatabaseCreator.Model
     {
         [Key]
         public int Id { get; set; }
+        [MaxLength(40)]
         public string Name { get; set; }
+        [MaxLength(100)]
         public string? Description { get; set; }
     }
 }
--- MediaDatabaseCreator/MediaDatabaseCreator/Model/Movie.cs
+++ MediaDatabaseCreator/MediaDatabaseCreator/Model/Movie.cs
@@ -6,9 +6,12 @@ namespace MediaDatabaseCreator.Model
     {
         [Key]
         public int Id { get; set; }
+        [MaxLength(40)]
         public string MovieTitle { get; set; }
+        [MaxLength(60)]
         public string Genre { get; set; }
         public int ReleaseYear { get; set; }
+        [MaxLength(40)]
         public string Director { get; set; }
         public string MoviePicture { get; set; }
         public string MovieTrailer { get; set; }
