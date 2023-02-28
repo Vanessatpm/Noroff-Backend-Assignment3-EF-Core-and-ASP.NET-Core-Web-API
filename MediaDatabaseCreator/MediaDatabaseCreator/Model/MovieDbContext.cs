1ba632634035ad634839eb8dd99380523b1921ba
Vanessa Pastén-Millán <vane.tama@hotmail.com>
2023-02-28 14:33:55 +0100

    wip: add connection string, edit dbcontext constructor, and remove excess model file

--- MediaDatabaseCreator/MediaDatabaseCreator/Model/MovieDbContext.cs
+++ MediaDatabaseCreator/MediaDatabaseCreator/Model/MovieDbContext.cs
@@ -5,21 +5,11 @@ namespace MediaDatabaseCreator.Model
 {
     public class MovieDbContext : DbContext
     {
-        public MovieDbContext() : base()
-        {
-
-        }
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options) {}
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Character> Characters { get; set; }
         public DbSet<Franchise> Franchises { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(
-                "Data Source = " + //<INSERT YOUR DATA SOURCE>; " +
-                "Initial Catalog = MovieEF; " +
-                "Integrated Security = True; " +
-                "Trust Server Certificate = True;");
-        }
+        
     }
 }
