492ff4efb05f1acdfcdde7f2c8ef6a5a9e29f986
Vanessa Pastén-Millán <vane.tama@hotmail.com>
2023-03-03 17:17:37 +0100

    fix: add import

--- MediaDatabaseCreator/MediaDatabaseCreator/Controllers/MovieController.cs
+++ MediaDatabaseCreator/MediaDatabaseCreator/Controllers/MovieController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MediaDatabaseCreator.Model.Entities;
+using MediaDatabaseCreator.Model;
 
 namespace MediaDatabaseCreator.Controllers
 {
